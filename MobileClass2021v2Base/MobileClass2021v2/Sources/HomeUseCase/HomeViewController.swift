5bbf125a50e8f3d7fc70fb24c70527727652d709
Jhoney Lopes <jhoney.lopes@gmail.com>
2021-09-16 20:27:54 -0300

    [N/A] jhoneylopes Init class 2

--- MobileClass2021v2Base/MobileClass2021v2/Sources/HomeUseCase/HomeViewController.swift
+++ MobileClass2021v2Base/MobileClass2021v2/Sources/HomeUseCase/HomeViewController.swift
@@ -1,7 +1,14 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+
+    weak var nameTextField: UITextField!
+    weak var fullNameTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // var | let | func
     }
+
 }
